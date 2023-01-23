3a06b73320992127cdd27023f354262faa65d6cd
Tefe <122893444+Tefeogb@users.noreply.github.com>
2023-01-23 14:24:56 -0500

    Update index.js

--- js/index.js
+++ js/index.js
@@ -31,6 +31,6 @@ let email ="tefeogbekene@gmail.com";
 
 let sentence;
 
-sentence = 'My name is' + ' ' + first_name + ' ' + last_name + '.' + 'You can contact me at' + ' ' + email + ' ';
+output = 'My name is' + ' ' + first_name + ' ' + last_name + ' ' + '.' + 'You can contact me at' + ' ' + email + ' ';
 
-console.log(sentence);
+alert(output);
