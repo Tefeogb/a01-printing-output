a5d79700a240035579b4b85f996bbb0aabd7f6df
Tefe <122893444+Tefeogb@users.noreply.github.com>
2023-01-23 14:05:15 -0500

    concatenation updated

--- js/index.js
+++ js/index.js
@@ -8,5 +8,29 @@ output += 'let c;\n';
 output += '----------\n';
 output += 'a + b = ' + (a + b) + "\n";
 output += 'a - b = ' + (a - b) + "\n";
+output += 'a * b = ' + (a * b) + "\n";
+output += 'a / b = ' + (a / b) + "\n";
+output += 'a % b = ' + (a % b) + "\n";
+output += ('(a += b):') + (a += b) + "\n";
+output += ('(a +- b):') + (a +- b) + "\n";
+output += ('a *= b = ' + (a *= b) + "\n");
+output += ('a /= b = ' + (a /= b) + "\n");
+output += ('(a %= b):') + (a %= b) + "\n";
+output += ('(a == b):') + (a == b) + "\n";
+output += ('(a != b):') + (a != b) + "\n";
+output += ('(a > b):') + (a > b) + "\n";
+output += ('(a < b):') + (a < b) + "\n";
+output += ('(!a && !c):') + (!a && !c) + "\n";
+output += ('(!a || !c):') + (!a || !c) + "\n";
 
 alert(output);
+
+let first_name = "Tefe";
+let last_name = "Ogbekene";
+let email ="tefeogbekene@gmail.com";
+
+let sentence;
+
+sentence = 'My name is' + ' ' + first_name + ' ' + last_name + '.' + ' ' + 'You can contact me at' + ' ' + email + ' ';
+
+console.log(sentence);
